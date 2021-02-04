e15835fd8f76e66e40b3238276aaeaf3881fc97b
BrandonGul <bsguly@gmail.com>
2021-02-04 17:40:16 -0600

    Added pictures to cool vids section Added solor section  - Has button that plays the solar.mp3 file
    Changed the menu button to pop the menu into frame when clicked
     - Fires the click.mp3 sound when clicked
    
    Added our team popup when our team link is clicked

--- js/bruh.js
+++ js/bruh.js
@@ -1,4 +1,5 @@
 var wavesSound = new Audio("./earThoughts/waves.mp3");
+var solar = new Audio("./earThoughts/solar.mp3")
 
 function waves() {
   wavesSound.loop = true;
@@ -7,4 +8,36 @@ function waves() {
 
 function buy() {
   alert("Thank you for your purchase.")
+}
+
+function clickSoundFunc() {
+  var clickSound = new Audio("./earThoughts/click.mp3");
+  clickSound.play();
+
+  var menu = document.getElementById("menu");
+  if (window.getComputedStyle(menu).visibility === "hidden") {
+    menu.style.visibility = "visible"
+  }
+  else {
+    menu.style.visibility = "hidden"
+  }
+}
+
+function showTeam() {
+  document.getElementById("team-menu").style.visibility = "visible";
+}
+
+function hideTeam() {
+  var clickSound = new Audio("./earThoughts/click.mp3");
+  clickSound.play();
+
+  var menu = document.getElementById("menu");
+  if (window.getComputedStyle(menu).visibility === "visible") {
+    menu.style.visibility = "hidden"
+  }
+  document.getElementById("team-menu").style.visibility = "hidden";
+}
+
+function solarRoofs() {
+  solar.play();
 }
